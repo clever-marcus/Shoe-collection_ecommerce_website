8a3fbd80315611cfbaa02c3e94f62eb4c66a3f8e
Marcus Were <71174834+clever-marcus@users.noreply.github.com>
2022-06-13 11:40:28 +0300

    Add files via upload

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,18 @@
+let menu = document.querySelector('#menu-bar');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () => {
+
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+
+}
+
+window.onscroll = () => {
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+}
+
 var updateBtns = document.getElementsByClassName('update-cart');
 
 for (let i = 0; i < updateBtns.length; i++) {
@@ -50,18 +65,8 @@ toggle.addEventListener('click', () => {
     document.body.classList.toggle('dark');
 })
 
-let menu = document.querySelector('#menu-bar');
-let navbar = document.querySelector('.navbar');
 
-menu.onclick = () => {
-    menu.classList.toggle('fa-times');
-    navbar.classList.toggle('active');
-}
 
-window.onscroll = () => {
-    menu.classList.remove('fa-times');
-    navbar.classList.remove('active');
-}
 
 
 
